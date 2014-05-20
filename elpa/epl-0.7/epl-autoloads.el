ac7bcc8e8e6e69458de9f9376a2df2c7a7766bdd
Tomas Kacur <tomas.kacur@keboola.com>
2014-05-20 12:09:38 +0200

    added elpa packages into repo

---
+++ elpa/epl-0.7/epl-autoloads.el
@@ -0,0 +1,18 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (21352 58581 535000
+;;;;;;  0))
+
+;;;***
+
+(provide 'epl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; epl-autoloads.el ends here
