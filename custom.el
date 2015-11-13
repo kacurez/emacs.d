7be22cfc59576b5a52d7b11e79eca0fae4a7adae
Tomas Kacur <tomas.kacur@keboola.com>
2015-11-13 09:55:14 +0100

    minor stuff

--- custom.el
+++ custom.el
@@ -7,13 +7,13 @@
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
    ["#212121" "#CC5542" "#6aaf50" "#7d7c61" "#5180b3" "#DC8CC3" "#9b55c3" "#bdbdb3"])
- '(bmkp-last-as-first-bookmark-file "/Users/tomaskacur/.emacs.d/bookmarks")
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(coffee-tab-width 2)
- ;'(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
  '(custom-safe-themes
    (quote
     ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "98e5e942303b4f356d6573009c96087f9b872f2fa258c673188d913f6faf17ea" "8cf56691a70156f611ac86d0bbcbc7dee7673df195de5918f34bfdc6814ffd39" default)))
  '(fci-rule-color "#2e2e2e")
+ '(flycheck-coffeelintrc "~/.emacs.d/coffeelint.json")
  '(safe-local-variable-values (quote ((no-byte-compile t))))
  '(vc-annotate-background "#3b3b3b")
  '(vc-annotate-color-map
