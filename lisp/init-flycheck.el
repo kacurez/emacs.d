b35a8a9188a75f4ed4acf67ff91c527b04b7bd45
Tomas Kacur <tomas.kacur@keboola.com>
2015-11-13 09:56:03 +0100

    flycheck update , dont use ac anymore

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -3,9 +3,21 @@
 
   ;; Override default flycheck triggers
   (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
-	flycheck-idle-change-delay 0.8)
+        flycheck-idle-change-delay 0.8)
 
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
+
+(require 'flycheck)
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+     '(javascript-jshint))
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+     '(json-jsonlist))
 
 
 (provide 'init-flycheck)
