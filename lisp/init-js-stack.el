52599f5e2f6c533dfa79cff87fc441714a594a68
Tomas Kacur <tomas.kacur@keboola.com>
2015-11-13 09:53:46 +0100

    use web-mode

--- lisp/init-js-stack.el
+++ lisp/init-js-stack.el
@@ -4,12 +4,21 @@
 ;(require-package 'tern-auto-complete)
 
 (add-hook 'js-mode-hook (lambda () (tern-mode t)))
-
-;; (eval-after-load 'tern
-;;    '(progn
-;;       (require 'tern-auto-complete)
-;;       (tern-ac-setup)))
-
+(add-hook 'jsx-mode-hook (lambda () (tern-mode t)))
+(add-hook 'web-mode-hook (lambda ()
+                           (define-key web-mode-map (kbd "C-c C-e") 'web-mode-element-close)
+                           (setq web-mode-enable-auto-closing nil)
+                           (setq web-mode-enable-auto-pairing t)
+                           (setq web-mode-content-types-alist
+                                 '(("jsx" . "\\.js[x]?\\'")))
+                           (setq web-mode-enable-auto-quoting nil)
+                           (setq web-mode-markup-indent-offset 2)
+                           (tern-mode t))
+          ;; (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
+          ;; (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
+          ;; (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
+          ;; (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
+          )
 
 (add-hook 'coffee-mode-hook (lambda () (tern-mode t)))
 
@@ -19,5 +28,21 @@
 (add-hook 'jsx-mode-hook 'highlight-symbol-mode )
 (add-hook 'jsx-mode-hook 'highlight-symbol-nav-mode )
 
+
+;; use web-mode for .jsx files
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(setq js-indent-level 2)
+
+
+
+;Highlight current HTML element (see web-mode-current-element-highlight-face)
+(setq web-mode-enable-current-element-highlight nil)
+
+;You can also highlight the current column with
+(setq web-mode-enable-current-column-highlight t)
+
+
 (setenv "PATH" (concat "/usr/local/bin/node:" (getenv "PATH")))
+
+
 (provide 'init-js-stack)
